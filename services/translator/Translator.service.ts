0d3e32b8f8c500ab9404183206922f725916c572
Olavo Parno <olavoparno@gmail.com>
2020-06-08 20:40:09 -0300

    refactor(project): try again

--- services/translator/Translator.service.ts
+++ services/translator/Translator.service.ts
@@ -1,28 +1,26 @@
 import translate from 'baidu-translate-api'
 import { Logger } from '../logging/Logging.logger'
-import { ITranslateResponse, ITranslateOptions } from '../../interfaces'
+import { ITranslateOptions } from '../../interfaces'
 
-export const translateService = ({ message, from, to }: ITranslateOptions): Promise<ITranslateResponse | Error> => {
-  return new Promise((resolve, reject) => {
-    return translate(message, {
-      from,
-      to,
-    })
-      .then(resolve)
-      .catch((error) => {
-        reject(
-          new Error(
-            JSON.stringify({
-              status: 500,
-              data: {
-                information: 'Internal server error.',
-                complementary: JSON.stringify(error),
-              },
-            }),
-          ),
-        )
-      })
+export const translateService = ({ message, from, to }: ITranslateOptions): Promise<translate.returnObject> => {
+  return translate(message, {
+    from,
+    to,
   })
+    .then((translateResponse) => {
+      return translateResponse
+    })
+    .catch((error) => {
+      throw new Error(
+        JSON.stringify({
+          status: 500,
+          data: {
+            information: 'Internal server error.',
+            complementary: JSON.stringify(error),
+          },
+        }),
+      )
+    })
 }
 
 export const translateTriage = ({ message, from, to }: ITranslateOptions): Promise<ITranslateOptions | Error> => {
