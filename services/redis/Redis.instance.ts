9342fb446f3ff5cbbe649f276983e344570196a7
Olavo Parno <olavoparno@gmail.com>
2020-06-09 09:22:17 -0300

    Revert "refactor(redis): remove config"
    This reverts commit 37bbcc76fc2c3049848ff6853e87c0b9e6935cd1.

--- services/redis/Redis.instance.ts
+++ services/redis/Redis.instance.ts
@@ -3,9 +3,9 @@ import { Logger } from '../logging/Logging.logger'
 
 export function RedisManager(): Redis.Redis {
   const redisClient = new Redis({
-    host: process.env.REDIS_HOST,
-    password: process.env.REDIS_PW,
-    port: (process.env.REDIS_PORT as unknown) as number,
+    host: process.env.REDIS_HOST || 'us1-good-mite-30424.lambda.store',
+    password: process.env.REDIS_PW || '590b6c96919d442ab9ddd2a27989df28',
+    port: ((process.env.REDIS_PORT as unknown) as number) || 30424,
     keepAlive: (false as unknown) as number,
     maxRetriesPerRequest: 2,
     enableReadyCheck: true,
