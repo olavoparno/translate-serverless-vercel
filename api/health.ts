f3349db93dcea30dfd519d3d9c7c988be135733f
Olavo Parno <olavoparno@gmail.com>
2020-06-09 10:10:55 -0300

    refactor(health): return error

--- api/health.ts
+++ api/health.ts
@@ -6,9 +6,14 @@ export default (req: NowRequest, res: NowResponse): void => {
     translate('Health is ok!', {
       from: 'en',
       to: 'rom',
-    }).then((response) => {
-      console.log(response.trans_result.dst)
-      res.status(200).json(response)
-    }),
+    })
+      .then((response) => {
+        console.log(response.trans_result.dst)
+        res.status(200).json(response)
+      })
+      .catch((error) => {
+        console.log(error)
+        res.status(200).json(error)
+      }),
   )
 }
