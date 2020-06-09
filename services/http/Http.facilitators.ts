86aabcf540fd63f10eeebb21c9143d072c7ffb6f
Olavo Parno <olavoparno@gmail.com>
2020-06-09 09:22:18 -0300

    Revert "build(project): try interfaces typo"
    This reverts commit cfb3c711f4283bc845fd250a5bf90488a3e699af.

--- services/http/Http.facilitators.ts
+++ services/http/Http.facilitators.ts
@@ -2,7 +2,7 @@ import fs from 'fs'
 import path from 'path'
 import { NowResponse, NowRequest } from '@now/node'
 import { Logger } from '../logging/Logging.logger'
-import { ITranslateOptions } from '../../interfaces'
+import { ITranslateOptions } from '../../index'
 
 export const returnHttpJson = (res: NowResponse, status: number, payload: unknown): NowResponse => {
   return res.status(status).json(payload)
