88442b660d6628f113d4bec506049bb1a51a2b0b
Olavo Parno <olavoparno@gmail.com>
2020-06-12 00:32:58 -0300

    refactor(project): add useless updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,27 @@
 import React, { useEffect, useState } from 'react'
+import { Container, Grid, Typography, Link, CardMedia, Card } from '@material-ui/core'
+import { makeStyles } from '@material-ui/styles'
 import axios from 'axios'
 import './App.css'
+
+const useStyles = makeStyles({
+  root: {
+    display: 'flex',
+    justifyContent: 'center',
+    width: '100%',
+    height: 'calc(80vh - 24px)',
+    padding: '1rem 0',
+  },
+  cardGrid: {
+    padding: '2rem 0',
+  },
+  media: {
+    padding: '0 1rem',
+    backgroundSize: 'contain',
+    height: '100%',
+    width: '80%',
+  },
+})
 
 const healthDict: Record<number, any> = {
   200: {
@@ -22,24 +43,33 @@ function App(): JSX.Element {
       setHealthResponse(res)
     }
     getHealthStatus()
-  }, [])
+  }, [setHealthResponse])
+  const classes = useStyles()
   return (
-    <main>
-      <h2>
-        The project is currently{' '}
-        {healthDict[healthResponse?.status] ? healthDict[healthResponse.status].text : healthDict[0].text}
-      </h2>
-      {healthResponse?.status === 500 ? (
-        <>
-          <h1>Please watch the cat carefully</h1>
-          <img src="https://http.cat/418" alt="Cat in a Teapot!" />
-          <p>
-            Please refer to the documentation
-            <a href="https://github.com/olavoparno/translate-serverless-now/blob/master/README.md">README</a>
-          </p>
-        </>
-      ) : null}
-    </main>
+    <Container className={classes.cardGrid} maxWidth="xl">
+      <Grid container justify="center" alignItems="center" direction="column">
+        <Typography component="h1" variant="h3">
+          The project is currently:{' '}
+          {healthDict[healthResponse?.status] ? healthDict[healthResponse.status].text : healthDict[0].text}
+        </Typography>
+        {healthResponse?.status === 500 ? (
+          <>
+            <Typography component="h2">Please watch the cat carefully</Typography>
+            <Card elevation={0} className={classes.root}>
+              <CardMedia className={classes.media} image="https://http.cat/418" title="Cat in a Teapot!" />
+            </Card>
+            <Typography component="p">
+              Please refer to the documentation
+              <Link href="https://github.com/olavoparno/translate-serverless-now/blob/master/README.md">README</Link>
+            </Typography>
+          </>
+        ) : (
+          <Card elevation={0} className={classes.root}>
+            <CardMedia className={classes.media} image="https://http.cat/200" title="Everything is fine!" />
+          </Card>
+        )}
+      </Grid>
+    </Container>
   )
 }
 
